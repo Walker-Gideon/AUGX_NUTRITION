3a4e836823fffd9208f4ed591925d4615992fb3f
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-15 13:09:25 +0000

    Set the layout for the HeroMain component

--- src/features/hero/components/HeroMain.jsx
+++ src/features/hero/components/HeroMain.jsx
@@ -1,3 +1,19 @@
+import Container from "/src/ui/Container";
+import Image from "/src/assets/img01.jpg";
+
 export default function HeroMain() {
-    return <div>HeroMain</div>;
+    return <Container classname={"w-full bg-green-500 px-36"}>
+        {/* heading */}
+        <h1>Fuel Your Body. Elevate Your Training.</h1>
+
+        {/* subHeading */}
+        <h2>Premium supplements designed to support
+muscle growth, recovery and performance.</h2>
+
+        {/* image */}
+        <img src={Image} alt="" />
+
+        {/* button */}
+        <button>Order Today</button>
+    </Container>;
 }
