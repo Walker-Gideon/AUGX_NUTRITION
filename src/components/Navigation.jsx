a3c5770e4ff86a1c56a1ad2a4b0e27134eae9ef2
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-15 18:00:08 +0000

    Update the Navigation with styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 export default function Navigation() {
     return (
-        <nav>
-            <ul className="flex gap-5">
+        <nav className={"border border-white rounded-full py-3 px-8 bg-white shadow-xl/20 transition-all group"}>
+            <ul className="flex items-center gap-5">
                 <li>Products</li>
                 <li>Benefits</li>
                 <li>About Us</li>
