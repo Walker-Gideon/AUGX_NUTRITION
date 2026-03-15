e7d24ac57ac02075b023e3f705ca108fc18f5f2f
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-15 12:53:31 +0000

    Update in the Header component

--- src/features/hero/components/HeroHeader.jsx
+++ src/features/hero/components/HeroHeader.jsx
@@ -1,3 +1,5 @@
+import Header from "../../../ui/Header";
+
 export default function HeroHeader() {
-    return <div>HeroHeader</div>;
+    return <Header classname={"h-15 w-full bg-yellow-500"}>HeroHeader</Header>;
 }
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,3 +1,10 @@
-export default function Header({ children, classname }) {
+export default function Header({ children, classname, main, jstext }) {
+    if (main) {
+        return (
+            <header className={`flex items-center justify-${jstext} ${classname}`}>{children}</header>
+        );
+    }
+
+    // Default
     return <header className={classname}>{children}</header>;
 }
