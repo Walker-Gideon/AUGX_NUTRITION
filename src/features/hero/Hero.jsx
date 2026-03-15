f7e70b7632a5accf18828ea5871a7740ee3384cd
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-15 13:56:09 +0000

    Refactoring of codes in the Hero section

--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -1,10 +1,35 @@
+import Main from "/src/ui/Main";
+import Flex from "/src/ui/Flex";
+import Group from "/src/ui/Group";
+import Image from "/src/ui/Image";
+import Button from "/src/ui/Button";
 import Container from "/src/ui/Container";
-import HeroMain from "./components/HeroMain";
+import HeaderTexts from "/src/ui/HeaderTexts";
+
+import Img from "/src/assets/img01.jpg";
 
 export default function Hero() {
   return (
     <Container classname={"h-screen w-full bg-red-500"}>
-      <HeroMain />
+      <Main classname={"w-full bg-green-500 px-36 absolute inset-0 h-screen"}>
+        <Flex jsText="between" cText="center" classname={"h-full"}>
+          <Group>
+            <HeaderTexts>
+              Fuel Your Body. Elevate Your Training.
+            </HeaderTexts>
+            <HeaderTexts secondary={true}>
+              Premium supplements designed to support
+              muscle growth, recovery and performance.
+            </HeaderTexts>
+            <Button classname={"bg-blue-500"}>
+              Order Now
+            </Button>
+          </Group>
+          <Group>
+            <Image source={Img} alternate="Image of Creatine" classname={"w-50 h-50"} />
+          </Group>
+        </Flex>
+      </Main>
     </Container>
   );
 }
