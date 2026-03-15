5e702b10cd351576487be6da9e7b1dd176d05fc6
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-15 12:58:57 +0000

    Update in the HeroHeader component

--- src/features/hero/components/HeroHeader.jsx
+++ src/features/hero/components/HeroHeader.jsx
@@ -1,5 +1,9 @@
-import Header from "../../../ui/Header";
+import Header from "/src/ui/Header";
 
 export default function HeroHeader() {
-    return <Header classname={"h-15 w-full bg-yellow-500"}>HeroHeader</Header>;
+    return <Header main={true} classname={"h-15 w-full bg-yellow-500 px-36"} jstext={"between"}>
+        <div>Logo</div>
+        <div>Navigation</div>
+        <div>Button</div>
+    </Header>;
 }
