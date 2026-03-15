f0cd6d8b7d14af8b3f23407e4199d2ac9fb12953
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-15 17:18:40 +0000

    Created the ActionBution component

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,20 +1,14 @@
-import { LuArrowDown } from "react-icons/lu";
-
 import Logo from "./Logo";
 import Header from "/src/ui/Header";
-import Button from "/src/ui/Button";
 import Navigation from "./Navigation";
+import ActionButton from "./ActionButton";
 
 export default function MainHeader() {
-    // transparent
     return (
         <Header main={true} classname={"h-15 w-full px-36 sticky top-3 z-50 bg-transparent"} jstext={"between"}>
             <Logo />
             <Navigation />
-            <Button primary={true} classname={"flex items-center gap-2"}>
-                Order Now 
-                <LuArrowDown size={18} />
-            </Button>
+            <ActionButton />
         </Header>
     );
 }
