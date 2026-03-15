1cd8cf0847eb1ea94a360a1e0f8bd6a79349c6fa
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-15 13:44:50 +0000

    Created the MainHeader component and Update in the LandingPageLayout

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,9 @@
+export default function MainHeader() {
+    return (
+        <div>
+            <div>Logo</div>
+            <div>Navigation</div>
+            <div>Button</div>
+        </div>
+    );
+}
--- src/layouts/landingPage/LandingPageLayout.jsx
+++ src/layouts/landingPage/LandingPageLayout.jsx
@@ -2,6 +2,7 @@ import CTA from "/src/features/cta/CTA";
 import Container from "/src/ui/Container";
 import Hero from "/src/features/hero/Hero";
 import Footer from "/src/features/footer/Footer";
+import MainHeader from "/src/components/MainHeader";
 import Products from "/src/features/products/Products";
 import Benefits from "/src/features/benefits/Benefits";
 import Testimonials from "/src/features/testimonials/Testimonials";
@@ -9,6 +10,7 @@ import Testimonials from "/src/features/testimonials/Testimonials";
 export default function LandingPageLayout() {
   return (
     <Container>
+      <MainHeader />
       <Hero />
       <Products />
       <Benefits />
