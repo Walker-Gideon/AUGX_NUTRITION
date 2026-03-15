cb8de6a55fcbcacc0d6cf043b114c1fe4a31b09b
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-15 13:35:27 +0000

    Created the Main component and Update in the HeroMain component

--- src/features/hero/components/HeroMain.jsx
+++ src/features/hero/components/HeroMain.jsx
@@ -1,28 +1,30 @@
+import Main from "/src/ui/Main";
 import Flex from "/src/ui/Flex";
 import Group from "/src/ui/Group";
 import Image from "/src/ui/Image";
 import Button from "/src/ui/Button";
-import Container from "/src/ui/Container";
 import HeaderTexts from "/src/ui/HeaderTexts";
 
 import Img from "/src/assets/img01.jpg";
 
 export default function HeroMain() {
-    return <Container classname={"w-full bg-green-500 px-36"}>
-        <Flex jsText="between" cText="center" classname={"h-full"}>
-            <Group>
-                <HeaderTexts>
-                    Fuel Your Body. Elevate Your Training.
-                </HeaderTexts>
-                <HeaderTexts secondary={true}>
-                    Premium supplements designed to support
-                    muscle growth, recovery and performance.
-                </HeaderTexts>
-                <Button classname={"bg-blue-500"}>Order Today</Button>
-            </Group>
-            <Group>
-                <Image source={Img} alternate="Image of Creatine" classname={"w-50 h-50"} />
-            </Group>
-        </Flex>
-    </Container>;
+    return (
+        <Main classname={"w-full bg-green-500 px-36"}>
+            <Flex jsText="between" cText="center" classname={"h-full"}>
+                <Group>
+                    <HeaderTexts>
+                        Fuel Your Body. Elevate Your Training.
+                    </HeaderTexts>
+                    <HeaderTexts secondary={true}>
+                        Premium supplements designed to support
+                        muscle growth, recovery and performance.
+                    </HeaderTexts>
+                    <Button classname={"bg-blue-500"}>Order Today</Button>
+                </Group>
+                <Group>
+                    <Image source={Img} alternate="Image of Creatine" classname={"w-50 h-50"} />
+                </Group>
+            </Flex>
+        </Main>
+    );
 }
