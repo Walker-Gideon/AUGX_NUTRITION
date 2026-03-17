23e1a7c868536d6fd7ce61a0bf6b0a9c981cf8cd
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-17 12:25:18 +0000

    Update in the HeroText component

--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -1,15 +1,9 @@
-import { LuArrowDown } from "react-icons/lu";
-import { CiDeliveryTruck } from "react-icons/ci";
-
 import Main from "/src/ui/Main";
 import Flex from "/src/ui/Flex";
 import Group from "/src/ui/Group";
 import Image from "/src/ui/Image";
-import Button from "/src/ui/Button";
-import Paragraph from "/src/ui/Paragraph";
 import Container from "/src/ui/Container";
-import HeaderTexts from "/src/ui/HeaderTexts";
-import ActionButton from "/src/components/ActionButton";
+import HeroText from "./components/HeroText";
 
 import Img from "/src/assets/img01.jpg";
 
@@ -18,31 +12,7 @@ export default function Hero() {
     <Container classname={"w-full min-h-screen flex flex-col justify-center"}>
       <Main classname={"w-full px-4 py-16 lg:px-36"}>
         <Flex jsText="between" cText="center" classname={"flex-col middle:flex-row text-center gap-12"}>
-          <Group classname={"w-full lg:w-2/3"}>
-            <HeaderTexts classname={"text-5xl lg:text-6xl text-primary font-headlines w-full"}>
-                <Paragraph>
-                    Fuel Your Body.
-                </Paragraph>
-                <Paragraph>
-                    Elevate Your Training.
-                </Paragraph>
-            </HeaderTexts>
-            <HeaderTexts secondary={true} classname={"py-6 medium:px-16 text-xl lg:text-2xl text-quaternary font-text w-full"}>
-              Premium supplements designed to support
-              muscle growth, recovery and performance.
-            </HeaderTexts>
-            <Flex classname={"items-center justify-center gap-2"}>
-                <ActionButton icon={false} padding={"py-3.5 px-8"} />
-                <Button classname={"border flex items-center gap-2 rounded-full py-3.5 px-4 cursor-pointer text-xs medium:text-sm uppercase font-semibold font-headlines group shadow-xl/30 whitespace-nowrap"}>
-                    Learn More
-                    <LuArrowDown size={18} className=" group-hover:animate-bounce transition-all" />
-                </Button>
-            </Flex>
-            <Paragraph classname={"text-sm text-primary font-text w-full my-5 flex items-center justify-center lg:justify-start gap-2 px-4"}>
-                Free delivery available
-                <CiDeliveryTruck size={18} className="" />
-            </Paragraph>
-          </Group>
+          <HeroText />
           <Group>
             <Image source={Img} alternate="Image of Creatine" classname={"medium:w-140 medium:h-110 w-80 h-80"} />
           </Group>
--- src/features/hero/components/HeroText.jsx
+++ src/features/hero/components/HeroText.jsx
@@ -1,6 +1,40 @@
+import { LuArrowDown } from "react-icons/lu";
+import { CiDeliveryTruck } from "react-icons/ci";
+
+import Flex from "/src/ui/Flex";
+import Span from "/src/ui/Span";
+import Group from "/src/ui/Group";
+import Button from "/src/ui/Button";
+import Paragraph from "/src/ui/Paragraph";
+import HeaderTexts from "/src/ui/HeaderTexts";
+import ActionButton from "/src/components/ActionButton";
+
 export default function HeroText() {
     return (
-        <>
-        </>
+        <Group classname={"w-full lg:w-2/3"}>
+            <HeaderTexts classname={"text-5xl lg:text-6xl text-primary font-headlines w-full"}>
+                <Paragraph>Fuel Your Body.</Paragraph>
+                <Paragraph>Elevate Your Training.</Paragraph>
+            </HeaderTexts>
+
+            <HeaderTexts secondary={true} classname={"py-6 medium:px-16 text-xl lg:text-2xl text-quaternary font-text w-full"}>
+              Premium supplements designed to support
+              muscle growth, recovery and performance.
+            </HeaderTexts>
+
+            <Flex classname={"items-center justify-center gap-2"}>
+                <ActionButton icon={false} padding={"py-3.5 px-8"} />
+
+                <Button classname={"border flex items-center gap-2 rounded-full py-3.5 px-4 cursor-pointer text-xs medium:text-sm uppercase font-semibold font-headlines group shadow-xl/30 whitespace-nowrap"}>
+                    <Span>Learn More</Span>
+                    <LuArrowDown size={18} className="group-hover:animate-bounce transition-all" />
+                </Button>
+            </Flex>
+
+            <Paragraph classname={"text-sm text-primary font-text w-full my-5 flex items-center justify-center lg:justify-start gap-2 px-4"}>
+                <Span>Free delivery available</Span>
+                <CiDeliveryTruck size={18} />
+            </Paragraph>
+        </Group>
     );
 }
