c4f7f5b50b621fe5db930206e37be907c247bec9
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-17 11:51:54 +0000

    Fixes of UI bugs in the MainHeader component

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,14 +1,17 @@
 import Logo from "./Logo";
+import Group from "/src/ui/Group";
 import Header from "/src/ui/Header";
 import Navigation from "./Navigation";
 import ActionButton from "./ActionButton";
 
 export default function MainHeader() {
     return (
-        <Header main={true} classname={"h-15 w-full lg:px-36 px-4 medium:px-16 sticky top-3 medium:top-6 z-50 bg-transparent"} jstext={"between"}>
-            <Logo />
-            {/* <Navigation /> */}
-            <ActionButton />
+        <Header classname={"h-15 w-full lg:px-36 px-4 medium:px-16  medium:top-6 z-50 bg-transparent sticky top-3"}>
+            <Group classname={"w-full flex items-center justify-between"}>
+                <Logo />
+                {/* <Navigation /> */}
+                <ActionButton />
+            </Group>
         </Header>
     );
 }
