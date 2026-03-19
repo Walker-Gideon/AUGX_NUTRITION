021178165ad621012af82fd8d6d5212f8aedca1f
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-19 18:37:49 +0000

    Adjustment in the Navigation componenet

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 export default function Navigation() {
     return (
-        <nav className={"hidden middle:flex w-[35%] lg:w-[30%] border border-white rounded-full py-3 px-8 bg-white shadow-xl/20 transition-all group"}>
+        <nav className={"hidden middle:flex w-[35%] lg:w-[30%] border border-white rounded-full py-3.5 px-8 bg-white shadow-xl/20 transition-all group font-headlines text-sm font-semibold"}>
             <ul className="w-full flex items-center justify-center text-center gap-5">
                 <li className="flex-1">Products</li>
                 <li className="flex-1">Benefits</li>
