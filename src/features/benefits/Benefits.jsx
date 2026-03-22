9757b09d7705f9ef2cffdb32de99b6a900fc2762
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-22 12:46:56 +0000

    Styling update in the Benefits section

--- src/features/benefits/Benefits.jsx
+++ src/features/benefits/Benefits.jsx
@@ -6,7 +6,7 @@ import BenefitsDisplay from "./components/BenefitsDisplay";
 
 export default function Benefits() {
   return (
-    <Container section="benefits" classname={"h-full w-full px-4 md:px-10 lg:px-16 pb-20 lg:pb-32 bg-background"}>
+    <Container section="benefits" classname={"h-full w-full px-4 md:px-10 lg:px-16 py-15 md:py-20 lg:py-32 bg-primary/20"}>
       <BenefitsHeader />
       <Group classname={"grid middle:grid-cols-5 gap-4"}>
         {benefits.map((benefit) => (
