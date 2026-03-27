1066e4f29103d654e8ca9ef91b840b0cb64853e0
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-27 12:52:22 +0000

    Adjustment and Update in the Logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,3 +1,20 @@
+import { Link } from "react-scroll";
+
+import Group from "/src/ui/Group";
+
 export default function Logo() {
-    return <div>AUCX</div>;
+    return (
+        <Group>
+            <Link
+                to="hero"
+                smooth={true}
+                spy={true}
+                duration={500}
+                offset={-100}
+                className={"font-headlines text-2xl font-bold cursor-pointer uppercase"}
+            >
+                AUGX
+            </Link>
+        </Group>
+    );
 }
