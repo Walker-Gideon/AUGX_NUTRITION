dc873750d33a1b5978b62a19ad88fb9517bb3caa
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-30 15:16:40 +0000

    Created the HiddenSections component

---
+++ src/components/HiddenSections.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-scroll";
+
+import Group from "/src/ui/Group";
+
+export default function HiddenSections({ setActiveIndex }) {
+    return (
+        <Group classname={"hidden"}>
+            <Link to="hero" spy={true} onSetActive={() => setActiveIndex(null)} />
+            <Link to="cta" spy={true} onSetActive={() => setActiveIndex(null)} />
+            <Link to="footer" spy={true} onSetActive={() => setActiveIndex(null)} />
+        </Group>
+    );
+}
